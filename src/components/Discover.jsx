66e332e2982dd0a6c322fe47edaa6beabc0fbc79
ryangholland <rghcode@gmail.com>
2023-10-01 16:06:52 -0400

    Random Word button fetches word and def, logs to console

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -11,11 +11,34 @@ import DiscoverWord from "./DiscoverWord";
 import ManageSearchIcon from "@mui/icons-material/ManageSearch";
 
 function Discover() {
+  function getRandomWord() {
+    fetch("https://random-word-api.herokuapp.com/word")
+      .then((response) => response.json())
+      .then((word) => {
+        fetchDefinition(word);
+      });
+  }
+
+  function fetchDefinition(word) {
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (Array.isArray(data)) {
+          console.log("Definition found. Stopping loop.");
+        } else {
+          getRandomWord();
+        }
+      });
+  }
+
   return (
     <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
       <Typography variant="h6">Discover</Typography>
       <hr style={{ width: "90%" }}></hr>
-      <Button variant="outlined" sx={{mb:2}}>Random Word</Button>
+      <Button variant="outlined" sx={{ mb: 2 }} onClick={() => getRandomWord()}>
+        Random Word
+      </Button>
       <FormControl variant="standard">
         <InputLabel htmlFor="input-with-icon-adornment">
           Search for a word
