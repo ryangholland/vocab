8ffe7d82f3a81127fdbb1f1f85b224f62d53bf20
ryangholland <rghcode@gmail.com>
2023-10-07 13:15:27 -0400

    Prompt user to add words if list is empty.

--- src/components/VocabList.jsx
+++ src/components/VocabList.jsx
@@ -6,9 +6,23 @@ function VocabList({ words, handleDeleteWord }) {
     <Box display={"flex"} flexDirection={"column"} alignItems={"center"}>
       <Typography variant="h6">My List</Typography>
       <hr style={{ width: "90%" }}></hr>
+      {words.length === 0 && (
+        <>
+          <Typography align="center">Nothing here.</Typography>
+          <Typography align="center">
+            Add words with the Discover feature!
+          </Typography>
+        </>
+      )}
       <Box sx={{ width: 1, px: 2, mt: 1 }}>
         {words.map((word) => {
-          return <ListWord key={word.id} word={word} handleDeleteWord={handleDeleteWord} />;
+          return (
+            <ListWord
+              key={word.id}
+              word={word}
+              handleDeleteWord={handleDeleteWord}
+            />
+          );
         })}
       </Box>
     </Box>
